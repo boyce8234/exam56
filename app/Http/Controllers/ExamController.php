0438da02cc37ed935d3ff4a52c694b8235a75c8d
boyce8234 <40749536+boyce8234@users.noreply.github.com>
2018-07-05 16:03:11 +0800

    laravel class 20180705

---
+++ app/Http/Controllers/ExamController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exam;
+use App\Http\Requests\ExamRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ExamController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        if ($user and $user->can('建立測驗')) {
+            $exams = Exam::orderBy('created_at', 'desc')
+                ->paginate(10);
+        } else {
+            $exams = Exam::where('enable', 1)
+                ->orderBy('created_at', 'desc')
+                ->paginate(2);
+        }
+
+        return view('exam.index', compact('exams'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function create()
+    {
+        return view('exam.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ExamRequest $request)
+    {
+        Exam::create($request->all());
+        return redirect()->route('exam.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Exam $exam)
+    {
+        return view('exam.show', ['exam' => $exam]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Exam $exam)
+    {
+        return view('exam.create', compact('exam'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ExamRequest $request, Exam $exam)
+    {
+        $exam->update($request->all());
+        return redirect()->route('exam.show', $exam->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Exam $exam)
+    {
+        $exam->delete();
+    }
+}
---
+++ resources/views/exam/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app') 
+@section('content')
+<h1>測驗一覽</h1><small>（共 {{$exams->total()}} 筆資料）</small>
+<ul class="list-group">
+    @forelse($exams as $exam)
+    <li class="list-group-item">
+        {{ $exam->created_at->format("Y年m月d日") }} -
+        <a href="exam/{{ $exam->id }}">
+                    {{ $exam->title }}
+                </a>
+        @if($exam->enable!=1) {{ bs()->badge()->text('關閉') }} @endif
+    </li>
+    @empty
+    <li class="list-group-item">尚無任何測驗</li>
+    @endforelse
+</ul>
+
+<!-- 分頁 -->
+<div class="my-3">
+        {{ $exams->links() }}
+</div>
+
+@endsection
